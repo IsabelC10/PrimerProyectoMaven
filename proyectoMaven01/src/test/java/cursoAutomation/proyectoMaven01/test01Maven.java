6bf3179eb6bb1584b4da37b92715bd4ddea76385
IsabelC10 <35697662+IsabelC10@users.noreply.github.com>
2024-11-20 15:17:39 -0600

    Actualización de clase

--- proyectoMaven01/src/test/java/cursoAutomation/proyectoMaven01/test01Maven.java
+++ proyectoMaven01/src/test/java/cursoAutomation/proyectoMaven01/test01Maven.java
@@ -1,9 +1,17 @@
 package cursoAutomation.proyectoMaven01;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class test01Maven {
 	
 	WebDriver driver;
+	
+	//Modificaciones 1
+	
+	//Nueva forma de medir el tiempo
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	
 }
